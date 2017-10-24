3640de078874a497d47aae896752ad57e528a56f
dingchaolin <18810042351@163.com>
2017-10-24 17:57:47 +0800

    feat:imageOpetions

---
+++ interface/image.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by chaolinding on 2017/10/24.
+ */
+
+export interface CutImage{
+    path: string|Buffer;
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+};
+
---
+++ lib/imageOperation.ts
@@ -0,0 +1,27 @@
+import {clearScreenDown} from "readline";
+/**
+ * Created by chaolinding on 2017/10/24.
+ */
+import {CutImage} from "../interface/image";
+import * as images from "images";
+import {ImagesStatic} from "images";
+
+export class ImageOperation{
+
+    private image: ImagesStatic;
+
+    constructor(path: string){
+        this.image = images(path);
+    }
+
+    getCutImage(cutParams: CutImage):ImagesStatic{
+        return images( this.image, cutParams.x, cutParams.y, cutParams.width, cutParams.height);
+    }
+
+    toBase64(){
+        return "data:image/jpg;base64," + this.image.encode('jpg').toString("base64") ;
+    }
+
+
+}
+
